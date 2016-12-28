102cefcbc559e47957a24ba4deb84fb39b163ffa
mugt <nwck.thomas@gmail.com>
2016-12-28 19:27:31 +0100

    gestion d'erreur dans le formulaire

--- front/models.py
+++ front/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import RegexValidator
 
 class Contacte(models.Model):
-    nom = models.CharField(max_length=80)
-    prenom = models.CharField(max_length=80)
-    message = models.TextField(null=True)
-    telephone = models.PositiveIntegerField()
-    ville = models.CharField(max_length=90)
-    rue = models.CharField(max_length=120)
-    numero_de_la_voie = models.PositiveIntegerField()
-    code_postal = models.PositiveIntegerField()
-    mail = models.EmailField()
+    nom = models.CharField(max_length=80 , null=False)
+    prenom = models.CharField(max_length=80 , null=False)
+    message = models.TextField(null=True , blank=True)
+    telephone = models.CharField(max_length=10 , validators=[RegexValidator(regex='^\d{10}$', message='Veuiller donner un téléphone valide', code='Invalid number')] , null=False)
+    ville = models.CharField(max_length=90 , null=False)
+    rue = models.CharField(max_length=120 , null=False)
+    numero_de_la_voie = models.PositiveIntegerField( null=False)
+    code_postal = models.IntegerField(max_length=5 , validators=[RegexValidator(regex='^\d{5}$', message='Veuiller donner un code postal valide', code='Invalid postal')] , null=False)
+    mail = models.EmailField( blank=True)
     date = models.DateTimeField(auto_now_add=True, auto_now=False,
                                 verbose_name="Date de la demende")
 
