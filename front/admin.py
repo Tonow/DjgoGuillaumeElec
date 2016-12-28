9ac566f84f51a350544684468075635993c73143
mugt <nwck.thomas@gmail.com>
2016-12-28 13:06:36 +0100

    Ajout favicon

--- front/admin.py
+++ front/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Contacte
 
 class ContacteAdmin(admin.ModelAdmin):
-    list_display   = ('nom', 'prenom', 'date')
+    list_display   = ('id', 'nom', 'prenom', 'ville', 'date')
     list_filter    = ('nom','ville',)
     date_hierarchy = 'date'
     ordering       = ('date', 'nom' , 'ville' )
