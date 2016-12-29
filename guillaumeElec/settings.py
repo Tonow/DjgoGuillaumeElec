149891e001752bbfc5c48ed146f3dfb5bd040212
mugt <nwck.thomas@gmail.com>
2016-12-29 19:12:28 +0100

    changement des setting pour trouver les fichier static 5

--- guillaumeElec/settings.py
+++ guillaumeElec/settings.py
@@ -115,7 +115,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 """
-STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "sfiles"),
