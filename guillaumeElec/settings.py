abb801cd37d9fdee5dfac49a8618d0bff780f0d9
mugt <nwck.thomas@gmail.com>
2016-12-29 16:24:03 +0100

    changement des setting pour trouver les fichier statique en prod 2

--- guillaumeElec/settings.py
+++ guillaumeElec/settings.py
@@ -107,7 +107,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static' , BASE_DIR)
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
