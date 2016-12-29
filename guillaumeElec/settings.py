9c58b63b74b3e1fb17c6b95f705760c651d6d6b8
mugt <nwck.thomas@gmail.com>
2016-12-29 15:04:56 +0100

    ajout d'une page 404 et 500

--- guillaumeElec/settings.py
+++ guillaumeElec/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'o=cx9$a87d#*ax$knsg5w!=!obk50^wbt+a$+bw6kp7@*q)v6t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+#DEBUG = True
 
 ALLOWED_HOSTS = ["192.168.0.126"]
 #ALLOWED_HOSTS = []
