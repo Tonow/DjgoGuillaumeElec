6be72af7de1e33c20413b760b07b1bb1b580b9a3
mugt <nwck.thomas@gmail.com>
2016-12-29 17:19:09 +0100

    changement des setting pour trouver les fichier stat3 + test media phone

--- guillaumeElec/settings.py
+++ guillaumeElec/settings.py
@@ -106,9 +106,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_URL = '/static/'
+
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+
+"""
+##Test fichier static
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static' , BASE_DIR)
+
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
@@ -118,3 +131,4 @@ STATICFILES_DIRS = (
 #Indiquer les dossiers média
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR + '/media/'
+"""
