a6d56220726827b3c184ece521d0b09a66e60448
mugt <nwck.thomas@gmail.com>
2016-12-29 12:39:17 +0100

    modification de l'admin

--- guillaumeElec/settings.py
+++ guillaumeElec/settings.py
@@ -32,14 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'crispy_forms',
-    #'xadmin',
     'front',
 )
 
--- guillaumeElec/urls.py
+++ guillaumeElec/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
 
+
+from django.conf.urls import include, url
 from django.contrib import admin
-#import xadmin
-#xadmin.autodiscover()
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^xadmin/', include(xadmin.site.urls)),
+    #url(r'^sadmin/', include('suit.urls', 'suit')),
     url(r'^index/' , include('front.urls')),
 ]
