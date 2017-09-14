60bb04a111919c10959448d66a3ac4d223fea96c
mugt <nwck.thomas@gmail.com>
2017-09-14 13:26:51 +0200

    Docker ok pour site mais pas pour admin

--- guillaumeElec/settings.py
+++ guillaumeElec/settings.py
@@ -86,14 +86,17 @@ WSGI_APPLICATION = 'guillaumeElec.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 '''
+### SQL Lite ####################
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+#################################
 '''
-
+'''
+### Postgresql local ###########
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -104,7 +107,19 @@ DATABASES = {
         'PORT': '',
     }
 }
-
+#################################
+'''
+### Postgresql Docker ##########
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+#################################
 
 
 
